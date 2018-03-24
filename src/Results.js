4dbc8b281e84321f0560ff01a2ebe42f09f62ba0
cchungmin <min427@gmail.com>
2018-03-24 17:50:48 +0900

    First pass.

--- src/Results.js
+++ src/Results.js
@@ -3,53 +3,64 @@ import Masonry from 'react-masonry-component';
 import './Results.css';
 
 class Results extends Component {
-  getSizeClassName(num) {
-    let className = 'grid-item'
+  static getSizeClassName(num) {
+    const className = 'grid-item';
 
     if (num > 10000) {
-      return className + ' very-popular';
-    } else if (num < 10000 && num > 5000){
-      return className + ' popular';
+      return `${className} very-popular`;
+    } else if (num < 10000 && num > 5000) {
+      return `${className} popular`;
     }
 
     return className;
   }
 
   render() {
-    let options = {
+    const { results, onClick } = this.props;
+    const options = {
       itemSelector: '.grid-item',
       columnWidth: '.grid-sizer',
-      percentPosition: true
+      percentPosition: true,
     };
-
     return (
-      <Masonry options={options} elementType={'ul'} className="grid">
+      <Masonry options={options} elementType="ul" className="grid">
         <li className="grid-sizer"></li>
-        {this.props.results.map(item => {
-          return (
-              <SingleResult
-                key={item.id}
-                name={item.name}
-                mainCategoryName={item.categories[0].name}
-                count={item.stats.checkinsCount}
-                sizeClass={this.getSizeClassName(item.stats.checkinsCount)}
-                onClick={() => this.props.onClick(item)}/>
-          )
-        })}
+        {results.map(item => (
+          <SingleResult
+            key={item.id}
+            name={item.name}
+            mainCategoryName={item.categories[0].name}
+            count={item.stats.checkinsCount}
+            sizeClass={Results.getSizeClassName(item.stats.checkinsCount)}
+            onClick={() => onClick(item)}
+          />
+        ))}
       </Masonry>
     );
   }
-};
+}
 
-function SingleResult(props) {
+function SingleResult() {
+  const {
+    sizeClass,
+    name,
+    mainCategoryName,
+    count,
+    onClick,
+  } = this.props;
   return (
-    <li className={props.sizeClass} onClick={props.onClick}>
+    <li
+      className={sizeClass}
+      onClick={onClick}
+      onKeyDown={this.handleKeyDown}
+      role="menuitem"
+    >
       <div className="result-inner">
         <div className="result-inner-container">
-          {props.name}
+          {name}
         </div>
-        <p>{props.mainCategoryName}</p>
-        <span>{props.count} { props.count <= 1 ? 'Checkin' : 'Checkins'}</span>
+        <p>{mainCategoryName}</p>
+        <span>{count} { count <= 1 ? 'Checkin' : 'Checkins'}</span>
       </div>
     </li>
   );
